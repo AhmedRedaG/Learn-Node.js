e11833c8311f631e6bfe060ce2af2014710460ea
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-03-24 19:01:20 +0200

    module 3 task

---
+++ M3-Assignment/app.js
@@ -0,0 +1,90 @@
+const html = require("http");
+const fs = require("fs");
+
+const server = html.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  const url = req.url,
+    method = req.method;
+
+  if (url === "/") {
+    res.write(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+      </head>
+      <body>
+        <h1 style="padding:20px;text-align:center;margin:20px 0;">Welcome to our website!</h1>
+        <hr />
+        <p>Create new user</p>
+        <form action="/create-user" method="POST">
+          <input type="text" name="username" required>
+          <button type="submit">Submit</button>
+        </form>
+        <hr />
+        <p>View all users</p>
+        <form action="/users" method="POST">
+          <button type="submit">View Users</button>
+        </form>
+      </body> 
+      </html>`);
+    return res.end();
+  }
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => body.push(chunk));
+    return req.on("end", (err) => {
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split("=")[1];
+      console.log(`New user created: ${username}`);
+      // Save user data to a file
+      fs.appendFile("users.txt", username + "\n", (err) => {
+        if (err) throw err;
+        console.log("User saved successfully.");
+      });
+      res.writeHead(302, { Location: "/users" });
+      return res.end();
+    });
+  }
+  if (url === "/users") {
+    try {
+      const users = fs.readFileSync("users.txt", "utf8");
+      res.write(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Users</title>
+      </head>
+      <body>
+        <h2 style="padding:20px;text-align:center;margin:20px 0;">Users</h2>
+        <hr />
+        <ul>
+                  ${
+                    users
+                      .trim()
+                      .split("\n")
+                      .map((user) => `<li>${user}</li>`)
+                      .join("") || "no users yet"
+                  }
+        </ul>
+        <hr />
+        <form action="/" method="POST">
+          <button type="submit">Back to home</button>
+        </form>
+      </body>
+      </html>`);
+      return res.end();
+    } catch (err) {
+      console.error(err);
+      return res.end("Error reading users.txt");
+    }
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
